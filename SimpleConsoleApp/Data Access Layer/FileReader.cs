8b4c88d4e7b555665313e55638d16d8227f16f3b
Mo Mutlak <mo.mutlak@slicedbread.co.uk>
2017-02-26 23:39:02 +0000

    added in dictionary containing 350k words

--- SimpleConsoleApp/Data Access Layer/FileReader.cs
+++ SimpleConsoleApp/Data Access Layer/FileReader.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace SimpleConsoleApp.Data_Access_Layer
@@ -10,10 +12,24 @@ namespace SimpleConsoleApp.Data_Access_Layer
         {
             var wordArray = new List<string>();
 
-            foreach (var line in File.ReadLines(@"C:\Users\Mo Mutlak\Documents\Visual Studio 2013\Projects\hangman-game\SimpleConsoleApp\Dictionary\dictionary.txt", 
-                Encoding.UTF8))
+            try
             {
-                wordArray.Add(line);
+                var loadedFile =
+                    File.ReadLines(
+                        @"C:\Users\Mo Mutlak\Documents\Visual Studio 2013\Projects\hangman-game\SimpleConsoleApp\Dictionary\dictionary.txt",
+                        Encoding.UTF8).ToList();
+
+
+                foreach (var line in loadedFile)
+                {
+                    wordArray.Add(line);
+                }
+
+                return wordArray;
+            }
+            catch (DirectoryNotFoundException exception)
+            {
+                Console.WriteLine("Could not find a data dictionary to read!");
             }
 
             return wordArray;
